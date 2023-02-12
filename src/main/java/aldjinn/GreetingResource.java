c752058667b402a691a3ec271adbb3ebc62bb240
Dennis Wilmsmann <denniswilmsmann@gmx.de>
2023-02-12 23:04:17 +0100

    playing around

--- src/main/java/aldjinn/GreetingResource.java
+++ src/main/java/aldjinn/GreetingResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 @Path("/hello")
 public class GreetingResource {
@@ -11,7 +12,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Message hello() {
-        return new Message("Hello, world!");
+        return new Message("Hello, world! " + UUID.randomUUID());
     }
 
     private record Message(String body) {
